aefd577a7bf5d50e364589960e9a4769073e4bcc
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2022-07-02 06:16:49 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,31 +10,27 @@
 
 int main(void)
 {
-	int i, j;
+int i, j;
 
-	i = 48;
-	j = 48;
+i = 48;
+j = 48;
 
-	while (i < 58)
-	{
-		j = i + 1;
-		while (j < 58)
-		{
-			putchar(i);
-			putchar(j);
-
-			if (i < 56 || j < 57)
-			{
-				putchar(44);
-				putchar(32);
-			}
-			j++;
-		}
-
-		i++;
-	}
-
-	putchar(10);
-
-	return (0);
+while (i < 58)
+{
+j = i + 1;
+while (j < 58)
+{
+putchar(i);
+putchar(j);
+if (i < 56 || j < 57)
+{
+putchar(44);
+putchar(32);
+}
+j++;
+}
+i++;
+}
+putchar(10);
+return (0);
 }
