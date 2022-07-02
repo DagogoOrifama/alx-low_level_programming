2aa8e3847a7db7aef78a97ff4cc17f05ab1ef293
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2022-07-02 06:35:58 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,24 +10,21 @@
 
 int main(void)
 {
-	int i, j;
+int i, j;
 
-	i = 97;
+    i = 97;
 	j = 65;
 
-	while (i < 123)
-	{
-		putchar(i);
-		i++;
-	}
-
-	while (j < 91)
-	{
-		putchar(j);
-		j++;
-	}
-
-	putchar(10);
-
-	return (0);
+while (i < 123)
+{
+putchar(i);
+i++;
+}
+while (j < 91)
+{
+putchar(j);
+j++;
+}
+putchar(10);
+return (0);
 }
