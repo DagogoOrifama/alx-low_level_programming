b813e1d54f1ce333c068752c2df662dc269d61c6
Dagogo Orifama <dagoris2010@gmail.com>
2022-07-05 18:34:05 -0700

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints_alphabets prints lowercase alphabets
+ *
+ * Description: print_alphabet is a function that lowercase alphabets
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(j);
+	}
+	_putchar('\');
+
+}
