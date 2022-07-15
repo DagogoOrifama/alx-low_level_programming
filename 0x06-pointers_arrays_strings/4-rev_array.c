4db61cb38088ca6cb9bb0478b4cd6e25d4bbb993
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2022-07-15 05:16:07 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* reverse_array -> A function that reverses the content of an array of integers
+* reverse_array -> reverses the content of an array of integers
 * @a: array a
 * @n: an element of an array
 */
