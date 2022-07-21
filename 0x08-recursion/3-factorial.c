b20fb424fc3d07621461e77e87f01ecb9d5c1256
Dagogo Orifama <47601731+dagoris2010@users.noreply.github.com>
2022-07-21 04:38:20 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,15 +5,16 @@
 * @n: number
 * Return: an integer
 */
+
 int factorial(int n)
 {
 	if (n < 0)
-  {
-    return (-1);
-  }
-  	if (n <= 1)
-  {
-    return (1);
-  }
+	{
+		return (-1);
+	}
+	if (n <= 1)
+	{
+		return (1);
+	}
 	return (n * factorial(n - 1));
 }
