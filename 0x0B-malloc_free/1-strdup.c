9a7daa912ad2b685c72c513ca22c4790b1f17e20
Dagogo Orifama <dagoris2010@gmail.com>
2022-07-25 02:57:09 -0700

    added 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup -> string duplicator function
+ * @str: string to be duplicated
+ * Return: a string pointer
+ *
+ */
+
+char *_strdup(char *str)
+{
+	int m, n;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	for (m = 1; str[m]; m++)
+	{
+		s = (char *)malloc(m * sizeof(char) + 1);
+	}
+	if (s == NULL)
+		return (NULL);
+	for (n = 0; n < m; n++)
+	{
+		s[n] = str[n];
+	}
+	s[n] = '\0';
+	return (s);
+}
