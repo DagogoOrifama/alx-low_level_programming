124861067dba64e008f135ec845bf2585325ab93
Dagogo Orifama <dagoris2010@gmail.com>
2023-01-31 12:53:16 -0800

    update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,6 +25,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	for (i = 0; temp && i < idx; i++)
+	{
 		if (i == idx - 1)
 		{
 			new->next = temp->next;
